8ae03259ecbf0509a0f1880275f00b4d119d69d6
Lucas. <iaminvisy@gmail.com>
2024-10-02 02:01:49 +0100

    testing cdn
    but also getting images and info from a json and writing it

---
+++ fursonajson/script.js
@@ -0,0 +1,35 @@
+async function getData() {
+  const url = "./sonaart.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    
+    // https://jsfiddle.net/jbke43to/
+    Object.keys(json).forEach(function(key){
+      const cont = document.createElement('div')
+      cont.setAttribute('class', 'cont')
+
+      document.body.insertBefore(cont, document.querySelector('div#images'))
+      cont.innerHTML = `
+        <img src='${json[key].imgurl}' alt='${json[key].artist}' loading='lazy'>
+        <figcaption>
+          <a href='${json[key].artisturl}' target='_blank' rel='noopener noreferrer'>
+            @<u>${json[key].artist}</u>
+          </a>
+        </figcaption>
+      `
+    })
+
+    var el = document.querySelectorAll('figcaption');
+    for(var i=0;i<el.length;i++){
+      el[i].style.bottom = `${el[0].clientHeight}px`;
+    }
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getData()
